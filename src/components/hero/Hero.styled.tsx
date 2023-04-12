abe66e4b7be1beed79fcfde05ebca2e2449449c1
Pool <pool_1541@hotmail.com>
2023-04-12 11:26:08 -0500

    hero background fixed

--- src/components/hero/Hero.styled.tsx
+++ src/components/hero/Hero.styled.tsx
@@ -13,7 +13,8 @@ export const ImageContainer = styled.div`
   flex: 1 1 auto;
   img {
     max-width: 1400px;
-
+    height: 100vh;
+    max-height: 1600px;
     position: absolute;
     bottom: 0;
     left: -150px;
